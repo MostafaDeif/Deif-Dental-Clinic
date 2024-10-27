4f3c6eebd29dbed5e73877fa5b6e368a2562797d
MostafaDeif <mostafadeif11@gmail.com>
2024-10-27 23:18:15 +0300

    fixing

--- src/Components/Landing/index.jsx
+++ src/Components/Landing/index.jsx
@@ -1,5 +1,5 @@
 import "./index.scss"
-
+import Mohamed from "../../assets/Mohamed Deif.jpg"
 export default function Landing() {
 
     return (
@@ -7,24 +7,31 @@ export default function Landing() {
             <main>
                 <div className="left">
                     <div className="top">
-                        <p className="p1">WELCOME TO CONFIDENT</p>
-                        <h1>high quality & <span>painless</span> dentistry </h1>
-                        <p className="p2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud.</p>
+                        <p className="p1">WELCOME TO Deif Dental Clinic</p>
+                        <h1>For a <span>brighter</span>, whiter<span> smile</span>, discover the perfect care solution today!</h1>
+                        {/* <p className="p2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud.</p> */}
                     </div>
                     <div className="bottom">
-                        <button>CONTACT</button>
+                        <button>CONTACT US</button>
                     </div>
                 </div>
-                <div className="right">
+                {/* <div className="right">
                     <div className="c1">
                         <div  className="c2">
                             <div className="c3">
                                 <div  className="c4">
                                     <div  className="c5">
-                                        
+                                        <img src={test} alt="me" />
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div> */}
+                <div className="circle-container">
+                    <div className="circle">
+                        <div className="inner-circle">
+                            <img src={Mohamed} width="300" alt="Doctor Image" className="profile-img" />
                         </div>
                     </div>
                 </div>
