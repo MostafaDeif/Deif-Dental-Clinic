96da0042eb1590a93f05e5480708489df97be6f3
MostafaDeif <mostafadeif11@gmail.com>
2024-10-28 21:02:24 +0300

    nav edit

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,15 @@ export default function App() {
     <div className="col-12 App d-flex">
 
       <BrowserRouter>
-        
+    
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
-            {/* <Route path="join" element={<JoinPage />} />
-            <Route path="join/:join_type" element={<JoinPage />} /> */}
+            <Route path="contact_us" element={<h1>contact us</h1>} />
+            <Route path="about_us" element={<h1>About us</h1>} />
+            <Route path="services" element={<h1>Services</h1>} />
+            <Route path="book_now" element={<h1>book now</h1>} />
+            {/* <Route path="join/:join_type" element={<JoinPage />} /> */}
             <Route path="*" element={<h1>error 404</h1>} />
           </Route>
         </Routes>
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,27 +1,47 @@
-import "./index.scss"
+import { Link } from "react-router-dom";
+import "./index.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTooth } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
-    <nav className="col-12 d-flex justify-content-between align-items-center  ">
-      <div className="logo font-weight-bold"><i className="fa-solid fa-tooth"></i> Deif Dental Clinic</div>
-      <ul className="nav nav-pills">
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About Us</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Services</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact Us</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Book Now</a>
-        </li>
-      </ul>
-    </nav>
 
-  )
+        // <nav className="col-12 d-flex justify-content-between align-items-center  ">
+    //   <div className="logo font-weight-bold"><i className="fa-solid fa-tooth"></i> Deif Dental Clinic</div>
+    //   <ul className="nav nav-pills">
+    //     <li className="nav-item">
+    //       <a className="nav-link active" href="#">Home</a>
+    //     </li>
+    //     <li className="nav-item">
+    //       <a className="nav-link" href="#">About Us</a>
+    //     </li>
+    //     <li className="nav-item">
+    //       <a className="nav-link" href="#">Services</a>
+    //     </li>
+    //     <li className="nav-item">
+    //       <a className="nav-link" href="#">Contact Us</a>
+    //     </li>
+    //     <li className="nav-item">
+    //       <a className="nav-link" href="#">Book Now</a>
+    //     </li>
+    //   </ul>
+    // </nav>
+
+    <header>
+      <div className="logo">
+        <FontAwesomeIcon icon={faTooth} className="toothLogo" />
+        <p>Deif Dental Clinic</p>
+      </div>
+      {/* menu icon
+      <FontAwesomeIcon icon={faBars} className="menuIcon" /> */}
+      <nav>
+        <ul>
+          <li><Link to="/" className="routelink">Home</Link></li>
+          <li><Link to="about_us" className="routelink">About Us</Link></li>
+          <li><Link to="services" className="routelink">Services</Link></li>
+          <li><Link to="book_now" className="routelink">Book Now</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
